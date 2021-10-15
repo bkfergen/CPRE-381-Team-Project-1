b8edaae3b8f8da6f533a25b44a07e4858dd1f61a
bkfergen <bkfergen@iastate.edu>
2021-10-15 15:49:27 -0500

    changed ALU control lines into one input

--- src/MIPS_ALU.vhd
+++ src/MIPS_ALU.vhd
@@ -7,13 +7,14 @@ entity mips_alu is
   generic(N       : integer := 32);
   port(i_Data1    : in std_logic_vector(N-1 downto 0);    -- Data input 1
        i_Data2    : in std_logic_vector(N-1 downto 0);    -- Data input 2
-       i_C1       : in std_logic;                         -- Control 1 (1 = add/sub to output)
-       i_C2       : in std_logic;                         -- Control 2 (0 = add, 1 = sub)
-       i_C3       : in std_logic;                         -- Control 3 (1 = or to output)
-       i_C4       : in std_logic;                         -- Control 4 (1 = and to output)
-       i_C5       : in std_logic;                         -- Control 5 (1 = nor to output)
-       i_C6       : in std_logic;                         -- Control 6 (1 = xor to output)
-       i_C7       : in std_logic;                         -- Control 7 (1 = reql.qb to output)
+       i_C        : in std_logic_vector(7 downto 1);
+         -- Control(1) - (1 = add/sub to output)
+         -- Control(2) - (0 = add, 1 = sub)
+         -- Control(3) - (1 = or to output)
+         -- Control(4) - (1 = and to output)
+         -- Control(5) - (1 = nor to output)
+         -- Control(6) - (1 = xor to output)
+         -- Control(7) - (1 = reql.qb to output)
        o_Overflow : out std_logic;                        -- Overflow (1 = ovf, 0 = no ovf)
        o_Output   : out std_logic_vector(N-1 downto 0));  -- Data output
 
@@ -91,7 +92,7 @@ begin
   -- OPERATIONS
   ALU_Adder: addsub_N
   generic map(N => N)
-  port map(i_C => i_C2,
+  port map(i_C => i_C(2),
            i_I1 => i_Data1,
            i_I2 => i_Data2,
            o_S => s_ALU_Adder_Output,
@@ -125,42 +126,42 @@ begin
   -- DETERMINE OUTPUT
   ALU_Adder_Output_Mux: mux2t1_N
   generic map(N => N)
-  port map(i_S => i_C1,
+  port map(i_S => i_C(1),
            i_D0 => i_Data1,
            i_D1 => s_ALU_Adder_Output,
            o_O => s_Output1);
 
   ALU_Or_Output_Mux: mux2t1_N
   generic map(N => N)
-  port map(i_S => i_C3,
+  port map(i_S => i_C(3),
            i_D0 => s_Output1,
            i_D1 => s_ALU_Or_Output,
            o_O => s_Output2);
 
   ALU_And_Output_Mux: mux2t1_N
   generic map(N => N)
-  port map(i_S => i_C4,
+  port map(i_S => i_C(4),
            i_D0 => s_Output2,
            i_D1 => s_ALU_And_Output,
            o_O => s_Output3);
 
   ALU_Nor_Output_Mux: mux2t1_N
   generic map(N => N)
-  port map(i_S => i_C5,
+  port map(i_S => i_C(5),
            i_D0 => s_Output3,
            i_D1 => s_ALU_Nor_Output,
            o_O => s_Output4);
 
   ALU_Xor_Output_Mux: mux2t1_N
   generic map(N => N)
-  port map(i_S => i_C6,
+  port map(i_S => i_C(6),
            i_D0 => s_Output4,
            i_D1 => s_ALU_Xor_Output,
            o_O => s_Output5);
 
   ALU_Replqb_Output_Mux: mux2t1_N
   generic map(N => N)
-  port map(i_S => i_C7,
+  port map(i_S => i_C(7),
            i_D0 => s_Output5,
            i_D1 => s_ALU_Replqb_Output,
            o_O => s_Output6);
--- src/tb_MIPS_ALU.vhd
+++ src/tb_MIPS_ALU.vhd
@@ -14,13 +14,14 @@ architecture behavior of tb_mips_alu is
   generic(N       : integer := 32);
   port(i_Data1    : in std_logic_vector(N-1 downto 0);    -- Data input 1
        i_Data2    : in std_logic_vector(N-1 downto 0);    -- Data input 2
-       i_C1       : in std_logic;                         -- Control 1 (1 = add/sub to output)
-       i_C2       : in std_logic;                         -- Control 2 (0 = add, 1 = sub)
-       i_C3       : in std_logic;                         -- Control 3 (1 = or to output)
-       i_C4       : in std_logic;                         -- Control 4 (1 = and to output)
-       i_C5       : in std_logic;                         -- Control 5 (1 = nor to output)
-       i_C6       : in std_logic;                         -- Control 6 (1 = xor to output)
-       i_C7       : in std_logic;                         -- Control 7 (1 = reql.qb to output)
+       i_C        : in std_logic_vector(7 downto 1);
+         -- Control(1) - (1 = add/sub to output)
+         -- Control(2) - (0 = add, 1 = sub)
+         -- Control(3) - (1 = or to output)
+         -- Control(4) - (1 = and to output)
+         -- Control(5) - (1 = nor to output)
+         -- Control(6) - (1 = xor to output)
+         -- Control(7) - (1 = reql.qb to output)
        o_Overflow : out std_logic;                        -- Overflow (1 = ovf, 0 = no ovf)
        o_Output   : out std_logic_vector(N-1 downto 0));  -- Data output
   end component;
@@ -30,13 +31,7 @@ architecture behavior of tb_mips_alu is
   signal s_Data1 : std_logic_vector(31 downto 0);
   signal s_Data2 : std_logic_vector(31 downto 0);
   signal s_Output : std_logic_vector(31 downto 0);
-  signal s_C1 : std_logic;
-  signal s_C2 : std_logic;
-  signal s_C3 : std_logic;
-  signal s_C4 : std_logic;
-  signal s_C5 : std_logic;
-  signal s_C6 : std_logic;
-  signal s_C7 : std_logic;
+  signal s_C : std_logic_vector(7 downto 1);
   signal s_Overflow : std_logic;
 
 begin
@@ -45,13 +40,7 @@ begin
   generic map(N => 32)
   port map(i_Data1 => s_Data1,          -- Data input 1
            i_Data2 => s_Data2,          -- Data input 2
-           i_C1 => s_C1,                -- Control 1 (1 = add/sub to output)
-           i_C2 => s_C2,                -- Control 2 (0 = add, 1 = sub)
-           i_C3 => s_C3,                -- Control 3 (1 = or to output)
-           i_C4 => s_C4,                -- Control 4 (1 = and to output)
-           i_C5 => s_C5,                -- Control 5 (1 = nor to output)
-           i_C6 => s_C6,                -- Control 6 (1 = xor to output)
-           i_C7 => s_C7,                -- Control 7 (1 = reql.qb to output)
+           i_C => s_C,                  -- Control
            o_Overflow => s_Overflow,    -- Overflow (1 = ovf, 0 = no ovf)
            o_Output => s_Output);       -- Data output
 
@@ -72,94 +61,49 @@ begin
     -- run 325
     s_Data1 <= "00000000000000000000000000000000";
     s_Data2 <= "00000000000000000000000000000000";
-    s_C1 <= '0';
-    s_C2 <= '0';
-    s_C3 <= '0';
-    s_C4 <= '0';
-    s_C5 <= '0';
-    s_C6 <= '0';
-    s_C7 <= '0';
+    s_C <= "0000000";
     wait for cCLK_PER;
 
 -- Test add (s_Output should be "...0010"
     s_Data1 <= "00000000000000000000000000000001";
     s_Data2 <= "00000000000000000000000000000001";
-    s_C1 <= '1';
-    s_C2 <= '0';
-    s_C3 <= '0';
-    s_C4 <= '0';
-    s_C5 <= '0';
-    s_C6 <= '0';
-    s_C7 <= '0';
+    s_C <= "0000001";
     wait for cCLK_PER;
 
 -- Test sub (s_Output should be "...0000"
     s_Data1 <= "00000000000000000000000000000001";
     s_Data2 <= "00000000000000000000000000000001";
-    s_C1 <= '1';
-    s_C2 <= '1';
-    s_C3 <= '0';
-    s_C4 <= '0';
-    s_C5 <= '0';
-    s_C6 <= '0';
-    s_C7 <= '0';
+    s_C <= "0000011";
     wait for cCLK_PER;
 
 -- Test or (s_Output should be "...1011"
     s_Data1 <= "00000000000000000000000000001001";
     s_Data2 <= "00000000000000000000000000000011";
-    s_C1 <= '0';
-    s_C2 <= '0';
-    s_C3 <= '1';
-    s_C4 <= '0';
+    s_C <= "0000100";
     wait for cCLK_PER;
 
 -- Test and (s_Output should be "...0001"
     s_Data1 <= "00000000000000000000000000001001";
     s_Data2 <= "00000000000000000000000000000011";
-    s_C1 <= '0';
-    s_C2 <= '0';
-    s_C3 <= '0';
-    s_C4 <= '1';
-    s_C5 <= '0';
-    s_C6 <= '0';
-    s_C7 <= '0';
+    s_C <= "0001000";
     wait for cCLK_PER;
 
 -- Test nor (s_Output should be "1111...0100"
     s_Data1 <= "00000000000000000000000000001001";
     s_Data2 <= "00000000000000000000000000000011";
-    s_C1 <= '0';
-    s_C2 <= '0';
-    s_C3 <= '0';
-    s_C4 <= '0';
-    s_C5 <= '1';
-    s_C6 <= '0';
-    s_C7 <= '0';
+    s_C <= "0010000";
     wait for cCLK_PER;
 
 -- Test xor (s_Output should be "...1010"
     s_Data1 <= "00000000000000000000000000001001";
     s_Data2 <= "00000000000000000000000000000011";
-    s_C1 <= '0';
-    s_C2 <= '0';
-    s_C3 <= '0';
-    s_C4 <= '0';
-    s_C5 <= '0';
-    s_C6 <= '1';
-    s_C7 <= '0';
+    s_C <= "0100000";
     wait for cCLK_PER;
 
 -- Test repl.qb (s_Output should be "||10100011|| x 4"
     s_Data1 <= "00000000000000000000000000000000"; --Only uses imm (read into data 2)
     s_Data2 <= "00000000000000000000000010100011";
-    s_C1 <= '0';
-    s_C2 <= '0';
-    s_C3 <= '0';
-    s_C4 <= '0';
-    s_C5 <= '0';
-    s_C6 <= '0';
-    s_C7 <= '1';
+    s_C <= "1000000";
     wait for cCLK_PER;
 
     wait;
