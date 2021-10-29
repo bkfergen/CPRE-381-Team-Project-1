e47865f8607a878a48daf737f2648216ecf1ed48
bkfergen <bkfergen@iastate.edu>
2021-10-29 15:56:46 -0500

    added halt to alu, control

--- src/MIPS_Processor.vhd
+++ src/MIPS_Processor.vhd
@@ -71,7 +71,7 @@ architecture structure of MIPS_Processor is
   generic(N       : integer := 32);
   port(i_Data1    : in std_logic_vector(N-1 downto 0);    -- Data input 1
        i_Data2    : in std_logic_vector(N-1 downto 0);    -- Data input 2
-       i_C        : in std_logic_vector(12 downto 1);
+       i_C        : in std_logic_vector(13 downto 1);
          -- Control(1) - (1 = add/sub to output)
          -- Control(2) - (0 = add, 1 = sub)
          -- Control(3) - (1 = or to output)
@@ -84,7 +84,9 @@ architecture structure of MIPS_Processor is
          -- Control(10) - (1 = barrelshifter to output)
          -- Control(11) - (0 = signed shift (if right), 1 = unsigned)
          -- Control(12) - (0 = right shift, 1 = left)
+         -- Control(13) - (1 = activate halt code)
        o_Overflow : out std_logic;                        -- Overflow (1 = ovf, 0 = no ovf)
+       o_Halt     : out std_logic;                        -- Halt (1 = halt, 0 = no halt)
        o_Output   : out std_logic_vector(N-1 downto 0);   -- Data output
        o_Zero     : out std_logic);                       -- Zero (1 = branch, 0 = no branch)
   end component;
@@ -102,7 +104,7 @@ architecture structure of MIPS_Processor is
     Branch	: out std_logic;
     Jump	: out std_logic;
     sign	: out std_logic;
-    ALU_Op  : out std_logic_vector(12 downto 1));
+    ALU_Op  : out std_logic_vector(13 downto 1));
   end component;
 
   component registerfile is
@@ -154,7 +156,7 @@ architecture structure of MIPS_Processor is
   signal s_Jump : std_logic;
   signal s_RegDst : std_logic;
   signal s_sign : std_logic;
-  signal s_ALU_Op : std_logic_vector(12 downto 1);
+  signal s_ALU_Op : std_logic_vector(13 downto 1);
 
   signal s_Zero : std_logic;
   signal s_Output : std_logic_vector(N-1 downto 0);
