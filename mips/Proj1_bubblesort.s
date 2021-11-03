fb88ff9ef37b1d868b85cd31f2318a3c463d398f
nicahill <ncahill@iastate.edu>
2021-11-03 16:09:24 -0500

    mips assembly test files

--- mips/Proj1_bubblesort.s
+++ mips/Proj1_bubblesort.s
@@ -1,14 +1,40 @@
+# bubble sort
+x
+	.data
+Arr:		.word 		4,3,5,1,2
+
+	.text
+	.global  main
 main:
-addi $t0, $zero, 5
-loop:
-beq $t1, $t0, exit
-jal loop2
-jr loop3
-bne $t0, $t0, exit
-addi $t1, $t1, 1
-j loop
-loop2:
-addi $a0, $a0, 1
-loop3:
-addi $a1, $a1, 1
-exit:
+   ori	$t1, $zero, 0x0005
+   ori  $t0, $zero, $zero
+mainloop:
+   addi $t2, $t0, 1
+   beq  $t2, $t1, maindone
+
+   lui $at, 4097
+   ori $a0, $at, 16
+
+   ori $t3, $zero, $zero
+
+   jal passloop
+
+
+passloop:
+   lw  $s1, 0($a0)
+   lw  $s2, 4($a0)
+   slt $at, $s1, s2
+   bne $at, $zero, passswap
+
+passnext:
+   addiu $a0, $a0, 4
+   addiu $t0, $t0, 1
+   slt $at, $zero, $t0
+   
+passswap:
+   sw $s1, 4($a0)
+   sw $s0, 0($a0)
+   ori $t3, $zero, 1 
+   j passnext
+maindone:
+   j end
