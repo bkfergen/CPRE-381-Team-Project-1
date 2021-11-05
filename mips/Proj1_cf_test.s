8a78ebe237b0cf988d559710d0ff971b1a4c5670
bkfergen <bkfergen@iastate.edu>
2021-11-05 14:50:42 -0500

    added lui

--- mips/Proj1_cf_test.s
+++ mips/Proj1_cf_test.s
@@ -1,14 +1,17 @@
 main:
-addi $t0, $zero, 5
+	addi $t0, $zero, 5
 loop:
-beq $t1, $t0, exit
-jal loop2
-jr loop3
-bne $t0, $t0, exit
-addi $t1, $t1, 1
-j loop
+	beq $t1, $t0, exit
+	jal loop2
+	jal loop3
+	bne $t0, $t0, exit
+	addi $t1, $t1, 1
+	j loop
 loop2:
-addi $a0, $a0, 1
+	addi $a0, $a0, 1
+	jr $ra
 loop3:
-addi $a1, $a1, 1
+	addi $a1, $a1, 1
+	jr $ra
 exit:
+	halt
