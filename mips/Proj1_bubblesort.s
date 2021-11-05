9dc0e82e9a80ff7af2f454f2e0d130b480598322
nicahill <ncahill@iastate.edu>
2021-11-05 15:55:34 -0500

    bubble sort done

--- mips/Proj1_bubblesort.s
+++ mips/Proj1_bubblesort.s
@@ -1,40 +1,53 @@
 # bubble sort
-x
+
 	.data
 Arr:		.word 		4,3,5,1,2
 
 	.text
 	.global  main
 main:
-   ori	$t1, $zero, 0x0005
-   ori  $t0, $zero, $zero
-mainloop:
-   addi $t2, $t0, 1
-   beq  $t2, $t1, maindone
+   ori	$t1, $zero, 0x0005    #load 5 into $t1
 
-   lui $at, 4097
+mainloop:
+   addi $a1, $t1, -1 	      #decrement $a1
+   beq  $zero, $a1, maindone  #if $a1 is equal to zero jump to end
+
+   lui $at, 4097	      # la pseudoinstruction that loads the Arr address into $a0
    ori $a0, $at, 16
 
-   ori $t3, $zero, $zero
+   ori $t3, $zero, $zero      #load zero into $t3
 
-   jal passloop
+   jal passloop		      #jump to passloop
 
+   beq $zero, $t2, maindone   #if there was no swaps we are done(jump to end)
+
+   addi $t1,$t1,-1            #decrement $t1
+   beq $zero, $zero, mainloop #unconditional jump back to mainloop
+
+   j end		      #jump to end
 
 passloop:
-   lw  $s1, 0($a0)
-   lw  $s2, 4($a0)
-   slt $at, $s1, s2
-   bne $at, $zero, passswap
+   lw  $s1, 0($a0)            #load Arr[i] into $s1
+   lw  $s2, 4($a0)	      #load Arr[i+1] onto $s2
+   slt $at, $s1, $s2 	      #if $s1 is less than $s2 set $at to 1 otherwise 0
+   bne $at, $zero, passswap   #branch if $at is not equal to $zero
 
 passnext:
-   addiu $a0, $a0, 4
-   addiu $t0, $t0, 1
-   slt $at, $zero, $t0
+   addiu $a0, $a0, 4	      #iterate array pointer
+   addiu $a1, $a1, -1         #decrement number of loops 
+
+   slt $at, $zero, $a1	      #if $a1 is less than 0 set $at 
+   beq $at, $zero, passloop   #if $at is equal to 0 loop
+
+   jr $ra		      #jump to return address	
    
 passswap:
-   sw $s1, 4($a0)
-   sw $s0, 0($a0)
-   ori $t3, $zero, 1 
-   j passnext
+   sw $s1, 4($a0)	      #store Arr[i+1] in address loaded in $s1
+   sw $s2, 0($a0)	      #store Arr[i] in address loaded in $s2
+   ori $t2, $zero, 1 	      #tell main we did a swap
+   j passnext		      #goto pass next
+
 maindone:
    j end
+
+end:
